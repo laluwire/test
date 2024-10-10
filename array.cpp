393f8738b8b93143bec3920289952757b74637b5
Lalu wirie <165606169+laluwire@users.noreply.github.com>
2024-10-10 09:39:30 +0800

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //LATIHAN 1
+    int nilai[5]={9, 8, 4, 2, 5};
+
+    cout<<"==================\n";
+    cout<<"[      ARRAY      ]\n";
+    cout<<"==================\n";
+
+    cout<<nilai[0]<<"tampilakn angka 9 \n";
+    cout<<nilai[1]<<"tampilakn angka 8 \n";
+    cout<<nilai[2]<<"tampilakn angka 4 \n";
+    cout<<nilai[3]<<"tampilakn angka 2 \n";
+    cout<<nilai[4]<<"tampilakn angka 5 \n";
+    cout<<endl;
+
+//LATIHAN 2
+        int nilai2[5]={4,3,2,8,8};
+    for (int i=0; i<5; i++){
+        cout<<nilai2[i]<<" ";
+        cout<<endl;
+    }
+
+//TUGAS SELESAI
+    string biodata[3]={"nama: ","alamt: ","jurusan: "};
+cout<<endl;
+    cout<<biodata[0]<<"Riyo\n";
+    cout<<biodata[1]<<"Bebuak\n";
+    cout<<biodata[2]<<"Teknik informatika \n \n";
+
+    cout<<"=======";
+    cout<<"{ DONE }";
+    cout<< "======\n";
+
+}
